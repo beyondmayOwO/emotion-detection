32e6389651e8930b0a7793cc38b3ad902cfc8f3b
beyondmayOwO <78870914+beyondmayOwO@users.noreply.github.com>
2025-01-20 23:55:28 +0630

    Different emotion scores and the dominant emotion of the text using NLU API

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,3 +1,5 @@
+"""Extract the different emotion scores and the dominant emotion of the text using Watson NLU API"""
+
 import os
 import requests
 from dotenv import load_dotenv
@@ -13,7 +15,7 @@ def emotion_detector(text_to_analyze):
     header = {"Content-Type": "application/json"}
     myobj = {
         "text": text_to_analyze,
-        "features": {
+        "features": { 
             "keywords": {
                 "emotion": True
             }
@@ -21,4 +23,26 @@ def emotion_detector(text_to_analyze):
     }
     response = requests.post(url, headers=header, json=myobj, auth=('apikey', API_KEY), timeout=10) 
 
-    return response.json()
+    # Extract emotion scores
+    dict_response = response.json()
+    sadness_score = dict_response['keywords'][0]['emotion']['sadness']
+    joy_score = dict_response['keywords'][0]['emotion']['joy']
+    fear_score = dict_response['keywords'][0]['emotion']['fear']
+    disgust_score = dict_response['keywords'][0]['emotion']['disgust']
+    anger_score = dict_response['keywords'][0]['emotion']['anger']
+
+    emotion_scores = {
+        'sadness': sadness_score,
+        'joy': joy_score,
+        'fear': fear_score,
+        'disgust': disgust_score,
+        'anger': anger_score
+    }
+
+    # Max function for dictionary to find the dominant emotion, the emotion with the highest score
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+
+    # Add the dominant emotion to the dictionary
+    emotion_scores['dominant_emotion'] = dominant_emotion
+
+    return emotion_scores
